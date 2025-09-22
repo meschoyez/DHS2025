ee47d6f8bc11942aef64e2df70321b0dceb8f438
Maximiliano Eschoyez <maximiliano.eschoyez@unc.edu.ar>
2025-09-22 12:29:57 -0300

    App.py funcionando

--- src/main/python/App.py
+++ src/main/python/App.py
@@ -1,19 +1,25 @@
 import sys
 from antlr4 import *
-from compiladoresLexer  import compiladoresLexer
-# from compiladoresParser import compiladoresParser
+from compiladorLexer  import compiladorLexer
+from compiladorParser import compiladorParser
 
+# En caso de no poder ejecutar el programa Python por
+# problemas de version (error ATNdeserializer), se
+# pueden generar los archivos a mano.
+#
+# Ir a la carpeta donde esta el archivo .g4 y ejecutar 
+#     antlr4 -Dlanguage=Python3 -visitor compilador.g4 -o .
 
 def main(argv):
-    archivo = "input/entrada.txt"
+    archivo = "input/programa.txt"
     if len(argv) > 1 :
         archivo = argv[1]
     input = FileStream(archivo)
-    lexer = compiladoresLexer(input)
+    lexer = compiladorLexer(input)
     stream = CommonTokenStream(lexer)
-    # parser = compiladoresParser(stream)
-    # tree = parser.s()
-    # print(tree.toStringTree(recog=parser))
+    parser = compiladorParser(stream)
+    tree = parser.programa()
+    print(tree.toStringTree(recog=parser))
 
 if __name__ == '__main__':
     main(sys.argv)
