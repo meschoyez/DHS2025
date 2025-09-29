62cbf67e688f16d20f31832491e3096e922ec5ae
Maximiliano Eschoyez <maximiliano.eschoyez@unc.edu.ar>
2025-09-29 12:00:23 -0300

    Clase 29 Sep

--- src/main/python/App.py
+++ src/main/python/App.py
@@ -2,6 +2,7 @@ import sys
 from antlr4 import *
 from compiladorLexer  import compiladorLexer
 from compiladorParser import compiladorParser
+from Escucha import Escucha
 
 # En caso de no poder ejecutar el programa Python por
 # problemas de version (error ATNdeserializer), se
@@ -11,15 +12,19 @@ from compiladorParser import compiladorParser
 #     antlr4 -Dlanguage=Python3 -visitor compilador.g4 -o .
 
 def main(argv):
-    archivo = "input/programa.txt"
+    # archivo = "input/programa.txt"
+    archivo = "input/simple.txt"
     if len(argv) > 1 :
         archivo = argv[1]
     input = FileStream(archivo)
     lexer = compiladorLexer(input)
     stream = CommonTokenStream(lexer)
     parser = compiladorParser(stream)
+    escucha = Escucha()
+    parser.addParseListener(escucha)
     tree = parser.programa()
-    print(tree.toStringTree(recog=parser))
+    print(escucha)
+    # print(tree.toStringTree(recog=parser))
 
 if __name__ == '__main__':
     main(sys.argv)
